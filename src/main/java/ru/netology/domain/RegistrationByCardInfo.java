84fa171f9aa2db34fa7a296fbe98b4053910ceae
Alexandra <alexabelyatskaya@yandex.ru>
2021-12-23 12:43:58 +0300

    Добавлена аннотация

--- src/main/java/ru/netology/domain/RegistrationByCardInfo.java
+++ src/main/java/ru/netology/domain/RegistrationByCardInfo.java
@@ -1,10 +1,12 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class RegistrationByCardInfo {
     private final String city;
     private final LocalDate cardExpire;
